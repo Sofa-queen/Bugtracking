e4c4f4ef2c168cd6372463a68f50ad048008815e
yacha <sofa.zu@mail.ru>
2020-04-02 01:10:37 +0300

    новый файл:    config/packages/dev/web_profiler.yaml 	новый файл:    config/packages/test/web_profiler.yaml 	новый файл:    config/routes/dev/web_profiler.yaml 	новый файл:    src/DataFixtures/.UserFixtures.php.swo 	новый файл:    src/DataFixtures/.UserFixtures.php.swp 	новый файл:    src/DataFixtures/AppFixtures.php 	новый файл:    src/DataFixtures/UserFixtures.php 	новый файл:    src/Migrations/Version20200325184213.php 	новый файл:    src/Migrations/Version20200327162359.php 	новый файл:    src/Migrations/Version20200329173124.php 	новый файл:    src/Migrations/Version20200330104752.php 	новый файл:    src/Migrations/Version20200331113424.php 	новый файл:    src/Migrations/Version20200331122427.php 	новый файл:    src/Migrations/Version20200331123027.php 	новый файл:    src/Security/.LoginFormAuthenticator.php.swn 	новый файл:    src/Security/.LoginFormAuthenticator.php.swo 	новый файл:    src/Security/.LoginFormAuthenticator.php.swp 	новый файл:    src/Security/LoginFormAuthenticator.php

---
+++ src/Migrations/Version20200329173124.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200329173124 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE ticket (id INT AUTO_INCREMENT NOT NULL, project_id INT NOT NULL, name VARCHAR(255) NOT NULL, creator VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, addressee VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, file VARCHAR(255) DEFAULT NULL, INDEX IDX_97A0ADA3166D1F9C (project_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE ticket ADD CONSTRAINT FK_97A0ADA3166D1F9C FOREIGN KEY (project_id) REFERENCES projects (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE ticket');
+    }
+}
